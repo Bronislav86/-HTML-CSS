505a2288352bdbf82337fc7989fdf42c63a62921
Bronislav <shevchenko-bg@yandex.ru>
2025-10-02 21:55:29 +0300

    was remasrered css to scss in parts from navigation to cardsOfProducts

--- Lesson_5_Адаптивная верстка/HW5/scripts/script.js
+++ Lesson_5_Адаптивная верстка/HW5/scripts/script.js
@@ -1,9 +1,9 @@
-const menuActive = document.querySelector('.menu__active');
+const menuActive = document.querySelector('.menuActive');
 const burger = document.querySelector('.rightButtons__menu');
 
 
 function toggleMenu () {
-  menuActive.classList.toggle('hidden');
+  menuActive.classList.toggle('menuActive_hidden');
 }
 
 burger.addEventListener('click', toggleMenu);
